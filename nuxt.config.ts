876bd696dbc45d631efaa0e6b25f166ec5f20cde
nk4dev <nknighta@varius.technology>
2026-02-13 23:31:21 +0900

    feat: 🎸 add cloudflare lib & config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,20 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  nitro: {
+    prerender: {
+      autoSubfolderIndex: false,
+    },
+  },
   devServer: {
     port: 3000,
   },
-  modules: ['@nuxtjs/tailwindcss', '@clerk/nuxt'],
+  modules: ["@nuxtjs/tailwindcss", "@clerk/nuxt"],
   tailwindcss: {
-    cssPath: '~/assets/global.css'
+    cssPath: "~/assets/global.css",
   },
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  
+
   // Vite configuration to handle dev server requests
   vite: {
     server: {
       // Suppress warnings for directory listing requests
       middlewareMode: false,
-    }
+    },
   },
-})
+});
